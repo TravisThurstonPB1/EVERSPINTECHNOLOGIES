f292ee5b09f0f979a8dc9a451ded1ddf9434961f
Travis <82836737+TravisThurstonPB1@users.noreply.github.com>
2021-10-04 16:15:08 -0700

    Update Validation ASSEM
    Enhanced Performance by optimizing SAP queries

--- ValidationAutomationPRDO_ASSEMV1.py
+++ ValidationAutomationPRDO_ASSEMV1.py
@@ -74,113 +74,116 @@ def dataGather():
 
 	
 def dataParse():
-	print ("Starting Data Parse...")
-	connection = pypyodbc.connect('DRIVER={0}; SERVER=10.0.0.6\SAPB1_SQL; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
-	cursor = connection.cursor()
-	
-	try:
-		for x in preimportEVS1:
-			# print(x)
-			abi, assylot, ponum, waferlot, finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty  = x
-			test6 = ("select oitt.code, Itt1.Code 'CompItem' from EverspinTech.dbo.oitt with(nolock) inner join EverspinTech.dbo.ITT1 with(nolock) on oitt.code = itt1.father where OITT.code = '{0}'".format(finitem))
-			cursor.execute(test6)
-			testresult6 = cursor.fetchone()
-			if testresult6 != None:
-				verify = (abi, assylot, ponum, waferlot, testresult6[1], finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty)
-				parse1.append(verify)
-			else:
-				pass
-			
-		#-- Checking for Report Complete --#			
-		for x in parse1:
-			abi, assylot, ponum, waferlot, ewsitem, finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty = x
-#			print(x)
-			test2 = ("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, ibt1.Quantity, ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202 where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and OWOR.Status = 'R' ".format(ponum, finitem, assylot))
-			cursor.execute(test2)
-			result = cursor.fetchone()
-			if result != None:
-				if result[5] == shipqty:
-					pass
-					# reason = (abi, ponum, finitem, assylot, shipqty, "AssyLot already received. Line 96", '0', '0')
-					# manualSAP.append(reason)
-				elif result[5] < shipqty:
-					compqty = shipqty - result[5]
-					verify = (abi, result[0], ponum, ewsitem, finitem, assylot, waferlot, assystqty, compqty, whsstart, whsfinish, 'Complete', scrpqty)
-					insertSAPComp.append(verify)
-				else:
-					reason = (abi, ponum, finitem, assylot, shipqty, "Greater amount completed in SAP than Spinweb.  Line 103", result[0], '0')
-					manualSAP.append(reason)
-			else:
-				test5=("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, ibt1.Quantity 'Quantity', ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202  where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and ibt1.Quantity = '{3}' and OWOR.Status = 'R'".format(ponum,ewsitem,waferlot,startqty))
-				cursor.execute(test5)
-				result4 = cursor.fetchone()
-				if result4 != None:
-					verify = (abi, result4[0], ponum, ewsitem, finitem, assylot, waferlot, assystqty, shipqty, whsstart, whsfinish, 'Complete', scrpqty)
-					insertSAPComp.append(verify)
-				else:
-					verify = (abi, waferlot, ewsitem, finitem)
-					unique.append(verify)
-		
-		parse2 = list(set(unique))
-		
-		#-- Checking for PRDO Creation --#
-		mysqlcon = pymysql.connect(user=mysqllogin.user, password=mysqllogin.password, host='10.10.60.198', port=3306, database='mtsdb')
-		cursor1 = mysqlcon.cursor()
-		for x in parse2:
-			abi, waferlot, ewsitem, finitem = x
-			query = ("Select distinct T1.ABI 'ABI', CASE WHEN ifnull(T2.PONumber,'') = '' THEN 0 ELSE T2.PONumber END as 'PONumber', concat(\"A_\",T1.assyLocation) 'WhsStart', 'T_UTC' as 'WhsFinish', ifnull(T2.dieQty,0) 'StartQty' from mtsdb.tblAssyLotInfo T1 left join mtsdb.tblABILog T2 on Case when T1.ABI like 'EABI%' then left(replace(replace(T1.ABI,'_',''),'-',''),18) else left(replace(replace(T1.ABI,'_',''),'-',''),17) end = replace(T2.ABI,'_','') where T1.ABI = '{0}' and T1.waferLotAll = '{1}'".format(abi,waferlot))
-			cursor1.execute(query)
-			results = cursor1.fetchone()
-			verify = (abi, results[1], finitem, results[4], results[3], ewsitem, results[2], waferlot)
-			parse3.append(verify)
-			
-		for x in parse3:
-			abi, ponum, finitem, startqty, whsfinish, ewsitem, whsstart, waferlot = x
-#			print(x)
-			test4 = ("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, sum(isnull(IGN1.Quantity,0)) 'Quantity', ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202 left join EverspinTech.dbo.IGN1 with(nolock) on  OWOR.DocEntry = IGN1.BaseEntry and IGN1.Basetype = 202 where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and ibt1.Quantity = '{3}' and OWOR.Status = 'L' group by OWOR.Docnum, U_SpinwebNo, U_PONum, owor.ItemCode, PlannedQty, BatchNum, Status".format(ponum, ewsitem, waferlot, startqty))
-			cursor.execute(test4)
-			result2 = cursor.fetchone()
-			if result2 != None:
-				pass
-				# reason = (abi, ponum, finitem, waferlot, startqty, "Assembly Lot has already been processed.  Line 134", result2[0], '0')
-				# manualSAP.append(reason)
-			else:
-				test3 = ("Select S0.ItemCode, S0.BatchNum 'SAP Lot', S0.WhsCode, isnull(S0.InQty,0)-isnull(S1.OutQty,0) 'OnHand' From (select ItemCode, Batchnum, whscode, sum(quantity) 'InQty' from EverspinTech.dbo.IBT1 with(nolock) where BaseType in (59,67,18,20,16,15,14,10000071) and Direction=0 AND Docdate <= convert(date, getdate(),112) group by ItemCode, BatchNum, WhsCode) S0 left Join (select Distinct ItemCode, BatchNum, WhsCode, sum(quantity) 'OutQty' from EverspinTech.dbo.IBT1 with(nolock) where BaseType in (60,67,19,21,15,10000071) and Direction=1 and DocDate <= convert(date, getdate(),112) Group by ItemCode, BatchNum, WhsCode) S1 on S0.ItemCode = S1.ItemCode and S0.BatchNum = S1.BatchNum and S0.WhsCode = S1.WhsCode Where isnull(S0.inqty,0) - isnull(s1.OutQty,0) <> 0 and S0.BatchNum = '{0}' and S0.ItemCode = '{1}'".format(waferlot, ewsitem))
-				cursor.execute(test3)
-				result3 = cursor.fetchone()
-				if result3 != None:
-					if result3[3] >= startqty:
-						verify = (abi, ponum, finitem, startqty, whsfinish, ewsitem, result3[2], waferlot) 
-						insertSAPCreate.append(verify)
-					else:
-						reason = (abi, ponum, ewsitem, waferlot, startqty, "Not Enough Quantity On Hand in SAP, cannot create PRDO.  Line 145", '0', '0')
-						manualSAP.append(reason)
-				else:
-					reason =(abi, ponum, ewsitem, waferlot, startqty, "LotNum joined to EWS Item Code does Not Exist in SAP, cannot create PRDO.  Line 148", '0', '0')
-					manualSAP.append(reason)
+    print ("Starting Data Parse...")
+    connection = pypyodbc.connect('DRIVER={0}; SERVER=EverspinSQL2\SAPB1_SQL02; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
+    cursor = connection.cursor()
+
+    try:
+        for x in preimportEVS1:
+        # print(x)
+            abi, assylot, ponum, waferlot, finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty  = x
+            test6 = ("select oitt.code, Itt1.Code 'CompItem' from EverspinTech.dbo.oitt with(nolock) inner join EverspinTech.dbo.ITT1 with(nolock) on oitt.code = itt1.father where OITT.code = '{0}'".format(finitem))
+            cursor.execute(test6)
+            testresult6 = cursor.fetchone()
+            if testresult6 != None:
+                verify = (abi, assylot, ponum, waferlot, testresult6[1], finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty)
+                parse1.append(verify)
+            else:
+                pass
+
+    #-- Checking for Report Complete --#			
+        for x in parse1:
+            abi, assylot, ponum, waferlot, ewsitem, finitem, tracecode, lottype, whsstart, whsfinish, startqty, assystqty, shipqty, scrpqty = x
+
+            test2 = ("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, ibt1.Quantity, ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202 where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and OWOR.Status = 'R' ".format(ponum, finitem, assylot))
+            cursor.execute(test2)
+            result = cursor.fetchone()
+            if result != None:
+                if result[5] == shipqty:
+                    pass
+                    # reason = (abi, ponum, finitem, assylot, shipqty, "AssyLot already received. Line 96", '0', '0')
+                    # manualSAP.append(reason)
+                elif result[5] < shipqty:
+                    compqty = shipqty - result[5]
+                    verify = (abi, result[0], ponum, ewsitem, finitem, assylot, waferlot, assystqty, compqty, whsstart, whsfinish, 'Complete', scrpqty)
+                    insertSAPComp.append(verify)
+                else:
+                    reason = (abi, ponum, finitem, assylot, shipqty, "Greater amount completed in SAP than Spinweb.  Line 103", result[0], '0')
+                    manualSAP.append(reason)
+            else:
+                test5=("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, ibt1.Quantity 'Quantity', ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202  where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and ibt1.Quantity = '{3}' and OWOR.Status = 'R'".format(ponum,ewsitem,waferlot,startqty))
+                cursor.execute(test5)
+                result4 = cursor.fetchone()
+                if result4 != None:
+                    verify = (abi, result4[0], ponum, ewsitem, finitem, assylot, waferlot, assystqty, shipqty, whsstart, whsfinish, 'Complete', scrpqty)
+                    insertSAPComp.append(verify)
+                else:
+                    verify = (abi, waferlot, ewsitem, finitem)
+                    unique.append(verify)
+
+        parse2 = list(set(unique))
+
+#-- Checking for PRDO Creation --#
+        mysqlcon = pymysql.connect(user=mysqllogin.user, password=mysqllogin.password, host='10.10.60.198', port=3306, database='mtsdb')
+        cursor1 = mysqlcon.cursor()
+        for x in parse2:
+            abi, waferlot, ewsitem, finitem = x
+            query = ("Select distinct T1.ABI 'ABI', CASE WHEN ifnull(T2.PONumber,'') = '' THEN 0 ELSE T2.PONumber END as 'PONumber', concat(\"A_\",T1.assyLocation) 'WhsStart', 'T_UTC' as 'WhsFinish', ifnull(T2.dieQty,0) 'StartQty' from mtsdb.tblAssyLotInfo T1 left join mtsdb.tblABILog T2 on Case when T1.ABI like 'EABI%' then left(replace(replace(T1.ABI,'_',''),'-',''),18) else left(replace(replace(T1.ABI,'_',''),'-',''),17) end = replace(T2.ABI,'_','') where T1.ABI = '{0}' and T1.waferLotAll = '{1}'".format(abi,waferlot))
+            cursor1.execute(query)
+            results = cursor1.fetchone()
+            verify = (abi, results[1], finitem, results[4], results[3], ewsitem, results[2], waferlot)
+            parse3.append(verify)
+
+        for x in parse3:
+            abi, ponum, finitem, startqty, whsfinish, ewsitem, whsstart, waferlot = x
+            #			print(x)
+            test4 = ("select owor.DocNum, owor.U_SpinwebNo, owor.U_PONum, owor.ItemCode, owor.PlannedQty, sum(isnull(IGN1.Quantity,0)) 'Quantity', ibt1.BatchNum, owor.Status from EverspinTech.dbo.OWOR with(nolock) inner join EverspinTech.dbo.IBT1 with(nolock) on OWOR.DocEntry = IBT1.BsDocEntry and IBT1.BsDocType = 202 left join EverspinTech.dbo.IGN1 with(nolock) on  OWOR.DocEntry = IGN1.BaseEntry and IGN1.Basetype = 202 where owor.U_PONum = '{0}' and ibt1.ItemCode = '{1}' and ibt1.BatchNum ='{2}' and ibt1.Quantity = '{3}' and OWOR.Status = 'L' group by OWOR.Docnum, U_SpinwebNo, U_PONum, owor.ItemCode, PlannedQty, BatchNum, Status".format(ponum, ewsitem, waferlot, startqty))
+            cursor.execute(test4)
+            result2 = cursor.fetchone()
+            if result2 != None:
+                pass
+                # reason = (abi, ponum, finitem, waferlot, startqty, "Assembly Lot has already been processed.  Line 134", result2[0], '0')
+                # manualSAP.append(reason)
+            else:
+                test3 = ("Select S0.ItemCode, S0.BatchNum 'SAP Lot', S0.WhsCode, isnull(S0.InQty,0)-isnull(S1.OutQty,0) 'OnHand' From (select ItemCode, Batchnum, whscode, sum(quantity) 'InQty' from EverspinTech.dbo.IBT1 with(nolock) where BaseType in (59,67,18,20,16,15,14,10000071) and Direction=0 AND Docdate <= convert(date, getdate(),112) group by ItemCode, BatchNum, WhsCode) S0 left Join (select Distinct ItemCode, BatchNum, WhsCode, sum(quantity) 'OutQty' from EverspinTech.dbo.IBT1 with(nolock) where BaseType in (60,67,19,21,15,10000071) and Direction=1 and DocDate <= convert(date, getdate(),112) Group by ItemCode, BatchNum, WhsCode) S1 on S0.ItemCode = S1.ItemCode and S0.BatchNum = S1.BatchNum and S0.WhsCode = S1.WhsCode Where isnull(S0.inqty,0) - isnull(s1.OutQty,0) <> 0 and S0.BatchNum = '{0}' and S0.ItemCode = '{1}'".format(waferlot, ewsitem))
+                cursor.execute(test3)
+                result3 = cursor.fetchone()
+                if result3 != None:
+                    if result3[3] >= startqty:
+                        getitem=("Select ItemCode from OITM with(nolock) where ItemCode = '{0}'".format(finitem))
+                        cursor.execute(getitem)
+                        getitemcode = cursor.fetchone()
+                        verify = (abi, ponum, getitemcode[0], startqty, whsfinish, ewsitem, result3[2], waferlot) 
+                        insertSAPCreate.append(verify)
+                    else:
+                        reason = (abi, ponum, ewsitem, waferlot, startqty, "Not Enough Quantity On Hand in SAP, cannot create PRDO.  Line 145", '0', '0')
+                        manualSAP.append(reason)
+                else:
+                    reason =(abi, ponum, ewsitem, waferlot, startqty, "LotNum joined to EWS Item Code does Not Exist in SAP, cannot create PRDO.  Line 148", '0', '0')
+                    manualSAP.append(reason)
 
 
-	except:
-		print ("An error Occured with Parsing the Data.")
-		raise
+    except:
+        print ("An error Occured with Parsing the Data.")
+        raise
 
 #	print(manualSAP, "\n")
 #	print(insertSAPComp, "\n")
 #	input()
 #	print((x for x in insertSAPCreate), "\n")
 #	input()
-	print("Data Parsing Complete.")
+    print("Data Parsing Complete.")
 	
-	print("\n -----{} seconds------".format(time.time()-start_time))
-	cursor.close()
-	connection.close()
-	cursor1.close()
-	mysqlcon.close()
+    print("\n -----{} seconds------".format(time.time()-start_time))
+    cursor.close()
+    connection.close()
+    cursor1.close()
+    mysqlcon.close()
 	
 
 def createPRDOTbl ():
 	print("Starting PRDO Create Data insertion....")
-	connection = pypyodbc.connect('DRIVER={0}; SERVER=10.0.0.6\SAPB1_SQL; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
+	connection = pypyodbc.connect('DRIVER={0}; SERVER=EverspinSQL2\SAPB1_SQL02; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
 	cursor = connection.cursor()
 	try:
 		if insertSAPCreate != False:
@@ -224,7 +227,7 @@ def reportCompTbl():
 
 	
 	#### - Start MSSQL cursor connection - ####
-	connection = pypyodbc.connect('DRIVER={0}; SERVER=10.0.0.6\SAPB1_SQL; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
+	connection = pypyodbc.connect('DRIVER={0}; SERVER=EverspinSQL2\SAPB1_SQL02; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
 	cursor = connection.cursor()
 #	print("Starting Report Completion Data Insertion...")
 	mysqlcon = pymysql.connect(user=mysqllogin.user, password=mysqllogin.password, host='10.10.60.198', port=3306, database='mtsdb')
@@ -301,7 +304,7 @@ def manualEntry():
 	print("Starting Error Report Insertion...")
 
 	
-	connection = pypyodbc.connect('DRIVER={0}; SERVER=10.0.0.6\SAPB1_SQL; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
+	connection = pypyodbc.connect('DRIVER={0}; SERVER=EverspinSQL2\SAPB1_SQL02; DATABASE=EverspinTech; UID={1}; PWD={2}'.format('SQL Server',mysqllogin.mssql_user, mysqllogin.mssql_pass))
 	cursor = connection.cursor()
 		
 	try:
@@ -336,4 +339,6 @@ dataParse()
 createPRDOTbl()
 reportCompTbl()
 manualEntry()
+
+
 
